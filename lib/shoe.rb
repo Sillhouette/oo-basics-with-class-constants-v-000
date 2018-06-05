faaf3a7c589bcd1ec4a22b7b605bc8ae973a4da6
Richard Melchior <raustin.melchior@gmail.com>
2018-06-05 00:45:35 +0000

    automatically backed up by learn

--- lib/shoe.rb
+++ lib/shoe.rb
@@ -2,8 +2,6 @@ class Shoe
   attr_accessor :color, :size, :material, :condition
   attr_reader :brand
 
-  GENRES = ["Thriller", "Science Fiction", "Romance"];
-
   def initialize(brand)
     @brand = brand
   end
